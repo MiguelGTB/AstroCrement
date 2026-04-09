58ab8222ab64eb70717d1ff23a0cbe9013619784
miiguelgr04 <miguelgtb90@gmail.com>
2026-04-09 16:41:30 +0200

    Contador Dinero (Plasma Estelar)
    Función creada para que cuando se haga click en la Luna o Planeta aumente el contador del dinero

--- Assets/Scripts/ClickPlaneta.cs
+++ Assets/Scripts/ClickPlaneta.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ClickPlaneta : MonoBehaviour
@@ -7,13 +8,17 @@ public class ClickPlaneta : MonoBehaviour
     public int dineroActual = 0;
     public int dineroPorClic = 1;
 
-    // Creamos la función para que cada vez que clickemos en el Planeta (SphereCollider) aumente el dinero
+    // Interfaz
+    public TextMeshProUGUI textoDineroUI;
+
+    // Creamos la funciï¿½n para que cada vez que clickemos en el Planeta (SphereCollider) aumente el dinero
     private void OnMouseDown()
     {
         dineroActual += dineroPorClic;
 
+        textoDineroUI.text = "Plasma Estelar: " + dineroActual;
         // Enviamos un mensaje para corroborar que lo hicimos bien
-        Debug.Log("¡Clic en la Luna! Dinero total: " + dineroActual);
+        Debug.Log("ï¿½Clic en la Luna! Dinero total: " + dineroActual);
     }
 
 }
