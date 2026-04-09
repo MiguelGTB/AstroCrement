256ec4f7e03d76acd43bab3e06bc76b6984769df
miiguelgr04 <miguelgtb90@gmail.com>
2026-04-09 19:26:15 +0200

    Rebote Planeta
    Efecto de rebote al pulsar en un planeta

--- Assets/Scripts/ClickPlaneta.cs
+++ Assets/Scripts/ClickPlaneta.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
 using TMPro;
 using UnityEngine;
 
@@ -11,14 +12,32 @@ public class ClickPlaneta : MonoBehaviour
     // Interfaz
     public TextMeshProUGUI textoDineroUI;
 
+    // Efecto click planeta
+    private Vector3 tamanoOriginal;
+    private float velocidadRebote = 10f;
+    private float cantidadEncogimiento = 0.9f;
+
+
+    private void Start()
+    {
+        tamanoOriginal = transform.localScale;
+    }
+
+    private void Update()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, tamanoOriginal, Time.deltaTime * velocidadRebote);
+    }
+
+
     // Creamos la funci�n para que cada vez que clickemos en el Planeta (SphereCollider) aumente el dinero
     private void OnMouseDown()
     {
         dineroActual += dineroPorClic;
 
         textoDineroUI.text = "Plasma Estelar: " + dineroActual;
-        // Enviamos un mensaje para corroborar que lo hicimos bien
-        Debug.Log("�Clic en la Luna! Dinero total: " + dineroActual);
+
+        transform.localScale = tamanoOriginal * cantidadEncogimiento;
+
     }
 
 }
