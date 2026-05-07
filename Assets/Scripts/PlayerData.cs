bd8b27693383932f5407798cf3c231cfa0771104
davidrd32 <davidrd27125@gmail.com>
2026-05-07 20:45:09 +0200

    Configuración de la bd
    Inicio de sesion | formulario simple y guardado de partida y carga partida

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,11 @@
+[System.Serializable]
+public class PlayerData
+{
+    public double dineroActual;
+    public double dineroPorClic;
+    public double dineroPorSeg;
+    public int[] nivelesCompras;
+    
+    // Si más adelante quieres guardar las mejoras, añadiríamos:
+    // public bool[] mejorasCompradas;
+}
